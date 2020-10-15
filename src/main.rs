1d0fa22e08e44faa8167f55823ec3873bbc4c5c3
tayyeb70 <[tayyeb.javed546@gmail.com]>
2020-10-15 22:52:52 +0500

    fille initiated

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let x = 10;
+    let y  = 5;
+
+    let sum = x+y;
+    let minus = x-y;
+    let mul = x*y;
+    let divi = x/y;
+    let rem = x%y;
+
+    println!("{}",sum);
+    println!("{}",minus);
+    println!("{}",mul);
+    println!("{}",divi);
+    println!("{}",rem);
+
+}
